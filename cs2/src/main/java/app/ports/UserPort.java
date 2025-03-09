6cfadb8d28519c4e581806d15f184929dbc7c90d
JohanGarcia03 <johan.garcia15@correo.tdea.edu.co>
2025-03-09 12:17:42 -0500

    Se realizaron varios cambios dentro de esos cambios se creraron varias excepciones dentro de person y se organiza el codigo con algunos detalles.

--- cs2/src/main/java/app/ports/UserPort.java
+++ cs2/src/main/java/app/ports/UserPort.java
@@ -1,13 +1,12 @@
 package app.ports;
 
-import app.domain.models.Person;
 import app.domain.models.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserPort {
-    boolean existUserName(Long user);
+    boolean existUserName(String username);
     void save(User user);
     void delete(Long id);
     Optional<User> findById(Long id);
